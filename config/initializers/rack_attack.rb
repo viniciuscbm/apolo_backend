a46fd4ca7b1574b0c8e095536664cbd0ec51ae80
Vinicius Camboim <vini.camboim9@gmail.com>
2020-11-21 23:37:01 -0300

    api versioning

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,4 +2,4 @@
 Rack::Attack.safelist('allow-localhost') { |req| req.ip == '127.0.0.1' || req.ip == '::1' }
 
 # Allow an IP address to make 10 requests every 1 seconds
-Rack::Attack.throttle('req/ip', limit: 10, period: 1) { |_req| rep.ip }
+Rack::Attack.throttle('req/ip', limit: 10, period: 1, &:ip)
